f42b63fe0e4e3681466d1d7d89734ba45f3d693e
Vitaliy <egoshinvit@gmail.com>
2025-03-10 20:42:17 +0700

    add login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,3 +30,5 @@ export default function RootLayout({
     </html>
   )
 }
+
+//todo add container
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,23 @@
+'use client'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { LoginArgs } from '@/types/loginTypes'
+import s from './login.module.scss'
+import { Button } from '@/components/buttons/Button/Button'
+
 export default function Login() {
-  return <div>Login</div>
+  const { register, handleSubmit, reset } = useForm<LoginArgs>()
+
+  const onSubmit: SubmitHandler<LoginArgs> = (data) => {
+
+  }
+
+  return (
+    <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+      <h2>Authorization</h2>
+      <input className={s.input} type="email" placeholder="Email" />
+      <input className={s.input} type="password" placeholder="Password" />
+      <span className={s.checkBoxPlaceholder}>Remember me <input type="checkbox" /></span>
+      <Button title={'Login'} onClick={() => {}} />
+    </form>
+  )
 }
