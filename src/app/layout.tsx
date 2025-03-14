82cff7b049ae74a4194d737a72710302d68d4662
Vitaliy <egoshinvit@gmail.com>
2025-03-14 21:47:31 +0700

    vercel fix module scss styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Geist_Mono, Sigmar } from 'next/font/google'
-import './globals.module.scss'
+import './global.scss'
 import StoreProvider from '@/app/store/StoreProvider'
 import React from 'react'
 import { Header } from '@/components/Header/Header'
